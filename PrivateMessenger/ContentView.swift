bae0d03cd4c45e30086a79688f690625a17a732e
vtodorov78 <vvtodorov12@gmail.com>
2024-08-12 18:40:27 +0300

    initial commit

---
+++ PrivateMessenger/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PrivateMessenger
+//
+//  Created by Vladimir Todorov on 12.08.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PrivateMessenger/PrivateMessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrivateMessengerApp.swift
+//  PrivateMessenger
+//
+//  Created by Vladimir Todorov on 12.08.24.
+//
+
+import SwiftUI
+
+@main
+struct PrivateMessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
