93f22b4999e492ee7fd2013e35cd3d45a71fae1e
vtodorov78 <vvtodorov12@gmail.com>
2024-08-13 10:34:42 +0300

    project setup

---
+++ PrivateMessenger/Core/Authentication/View/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  PrivateMessenger
+//
+//  Created by Vladimir Todorov on 13.08.24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
