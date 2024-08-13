b87d3acc17fb7a34fe67ff6eead99e22c4238a87
vtodorov78 <vvtodorov12@gmail.com>
2024-08-13 12:58:55 +0300

    add login view UI

--- PrivateMessenger/Core/Authentication/View/LoginView.swift
+++ PrivateMessenger/Core/Authentication/View/LoginView.swift
@@ -8,8 +8,109 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                // logo image
+                Image("messenger")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+                
+                // text fields
+                VStack(spacing: 12) {
+                    TextField("Enter your email", text: $email)
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray6))
+                        .clipShape(.rect(cornerRadius: 10))
+                        .padding(.horizontal, 24)
+                    
+                    SecureField("Enter your password", text: $password)
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray6))
+                        .clipShape(.rect(cornerRadius: 10))
+                        .padding(.horizontal, 24)
+                }
+                
+                // forgot password
+                Button {
+                    print("Forgot password")
+                } label: {
+                    Text("Forgot pasword?")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                        .padding(.top)
+                        .padding(.trailing, 28)
+                }
+                .frame(maxWidth: .infinity, alignment: .trailing)
+        
+                // login button
+                Button {
+                    print("Handle login")
+                } label: {
+                    Text("Login")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .foregroundStyle(.white)
+                        .frame(width: 360, height: 44)
+                        .background(Color(.systemBlue))
+                        .clipShape(.rect(cornerRadius: 10))
+                }
+                .padding(.vertical)
+                
+                // facebook login
+                HStack {
+                    Rectangle()
+                        .frame(width: (UIScreen.main.bounds.width / 2) - 48, height: 0.5)
+                    
+                    Text("OR")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                    
+                    Rectangle()
+                        .frame(width: (UIScreen.main.bounds.width / 2) - 48, height: 0.5)
+                }
+                .foregroundColor(.gray)
+                
+                HStack {
+                    Image("apple")
+                        .resizable()
+                        .frame(width:25, height: 25)
+                        .clipShape(.circle)
+                    
+                    Text("Continue with Apple")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                        .foregroundStyle(Color(.black))
+                    
+                }
+                
+                Spacer()
+                
+                // sign in link
+                Divider()
+                
+                NavigationLink {
+                    Text("Sign up View")
+                } label: {
+                    HStack(spacing: 3) {
+                        Text("Don't have an account?")
+                        
+                        Text("Sign Up")
+                            .fontWeight(.semibold)
+                    }
+                    .font(.footnote)
+                }
+                .padding(.vertical
+                )
+            }
+        }
     }
 }
 
