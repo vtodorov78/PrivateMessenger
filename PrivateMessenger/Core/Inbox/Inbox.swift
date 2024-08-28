c053677ec8832f92ba8a3ff802026d9107de21ba
vtodorov78 <vvtodorov12@gmail.com>
2024-08-28 22:15:45 +0300

    add inbox view UI

---
+++ PrivateMessenger/Core/Inbox/Inbox.swift
@@ -0,0 +1,52 @@
+//
+//  Inbox.swift
+//  PrivateMessenger
+//
+//  Created by Vladimir Todorov on 14.08.24.
+//
+
+import SwiftUI
+
+struct Inbox: View {
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                ActiveNowView()
+                
+                List {
+                    ForEach( 0 ... 10, id: \.self) { message in
+                        InboxRowView()
+                    }
+                }
+                .listStyle(PlainListStyle())
+                .frame(height: UIScreen.main.bounds.height - 120)
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    HStack {
+                        Image(systemName: "person.circle.fill")
+                        
+                        Text("Chats")
+                            .font(.title)
+                            .fontWeight(.semibold)
+                    }
+                }
+                
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        print("Show message view")
+                    } label: {
+                        Image(systemName: "square.and.pencil.circle.fill")
+                            .resizable()
+                            .frame(width: 32, height: 32)
+                            .foregroundStyle(.black, Color(.systemGray5))
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    Inbox()
+}
---
+++ PrivateMessenger/Core/Inbox/InboxRowView.swift
@@ -0,0 +1,44 @@
+//
+//  InboxRowView.swift
+//  PrivateMessenger
+//
+//  Created by Vladimir Todorov on 28.08.24.
+//
+
+import SwiftUI
+
+struct InboxRowView: View {
+    var body: some View {
+        HStack(alignment: .top, spacing: 12) {
+            Image(systemName: "person.circle.fill")
+                .resizable()
+                .frame(width: 64, height: 64)
+                .foregroundStyle(Color(.systemGray4))
+            
+            VStack(alignment: .leading, spacing: 4) {
+                Text("Heath Ledger")
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                
+                Text("Some test message for now that spans more than one line")
+                    .font(.subheadline)
+                    .foregroundStyle(.gray)
+                    .lineLimit(2)
+                    .frame(maxWidth: UIScreen.main.bounds.width - 100, alignment: .leading)
+            }
+            
+            HStack {
+                Text("Yesterday")
+                
+                Image(systemName: "chevron.right")
+            }
+            .font(.footnote)
+            .foregroundStyle(.gray)
+        }
+        .frame(height: 72)
+    }
+}
+
+#Preview {
+    InboxRowView()
+}
