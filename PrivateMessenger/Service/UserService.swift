e661ee08a4cdde2cfd49accbffd12d20da94ff13
Vladimir Todorov <vladimirtodorov@Vladimirs-MacBook-Pro.local>
2025-02-09 16:49:16 +0200

    upload user data to Firebase Firestore

---
+++ PrivateMessenger/Service/UserService.swift
@@ -0,0 +1,24 @@
+//
+//  UserService.swift
+//  PrivateMessenger
+//
+//  Created by Vladimir Todorov on 9.02.25.
+//
+
+import Foundation
+import FirebaseAuth
+import FirebaseFirestore
+
+class UserService {
+    @Published var currentUser: User?
+    
+    static let shared = UserService()
+    
+    func fetchCurrentUser() async throws {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        let snapshot = try await Firestore.firestore().collection("users").document(uid).getDocument()
+        let user = try snapshot.data(as: User.self)
+        self.currentUser = user
+        print("DEBUG: Current user in service is \(currentUser)")
+    }
+}
