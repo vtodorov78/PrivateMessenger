e7249c2fb93ba9cae99ef95bb324a0cb31218ac6
Vladimir Todorov <vladimirtodorov@Vladimirs-MacBook-Pro.local>
2025-02-11 00:11:45 +0200

    add chat view navigation from inbox

--- PrivateMessenger/Core/Inbox/Views/InboxView.swift
+++ PrivateMessenger/Core/Inbox/Views/InboxView.swift
@@ -19,19 +19,31 @@ struct InboxView: View {
     
     var body: some View {
         NavigationStack {
-            ScrollView {
+            List {
                 ActiveNowView()
+                    .listRowSeparator(.hidden)
+                    .listRowInsets(EdgeInsets())
+                    .padding(.vertical)
+                    .padding(.horizontal, 4)
                 
-                List {
-                    ForEach(viewModel.recentMessages) { message in
+                ForEach(viewModel.recentMessages) { message in
+                    ZStack {
+                        NavigationLink(value: message) {
+                            EmptyView()
+                        }.opacity(0.0)
+                        
                         InboxRowView(message: message)
                     }
                 }
-                .listStyle(PlainListStyle())
-                .frame(height: UIScreen.main.bounds.height - 120)
             }
+            .listStyle(PlainListStyle())
             .onChange(of: selectedUser, { _, newValue in
                 showChat = newValue != nil
+            })
+            .navigationDestination(for: Message.self, destination: { message in
+                if let user = message.user {
+                    ChatView(user: user)
+                }
             })
             .navigationDestination(for: User.self, destination: { user in
                 ProfileView(user: user)
